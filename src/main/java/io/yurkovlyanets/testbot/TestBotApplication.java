a5dedd762544dfaaaec606eb9b9a4902dd04f895
yurkovlyanets <v.yurkovlyanets@gmail.com>
2023-05-08 23:26:56 +0600

    initial commit

---
+++ src/main/java/io/yurkovlyanets/testbot/TestBotApplication.java
@@ -0,0 +1,13 @@
+package io.yurkovlyanets.testbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/yurkovlyanets/testbot/TestBotApplicationTests.java
@@ -0,0 +1,13 @@
+package io.yurkovlyanets.testbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
